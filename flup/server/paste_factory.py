d2345e3aa268c0ae0c2989599635b5b45d641d97
Allan Saddi <allan@saddi.com>
2008-07-23 22:51:59 -0700

    Add support for configuring UNIX domain sockets (for servers that support them) in the paste.server_factory implementations.

--- flup/server/paste_factory.py
+++ flup/server/paste_factory.py
@@ -3,7 +3,11 @@ def helper(wsgiServerClass, global_conf, host, port, **local_conf):
     host = host or global_conf.get('host', 'localhost')
     port = port or global_conf.get('port', 4000)
 
-    local_conf['bindAddress'] = (host, int(port))
+    if 'socket' in local_conf:
+        local_conf['bindAddress'] = local_conf['socket']
+        del local_conf['socket']
+    else:
+        local_conf['bindAddress'] = (host, int(port))
     
     def server(application):
         server = wsgiServerClass(application, **local_conf)

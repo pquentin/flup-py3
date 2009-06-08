721d3503f26f455c3cfa682d28c232eb33dd5180
Allan Saddi <allan@saddi.com>
2009-06-08 10:28:30 -0700

    Bump version, configure setuptools for snapshots.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = 'flup',
-    version = '1.0.2',
+    version = '1.0.3',
     packages = find_packages(),
     zip_safe = True,
     

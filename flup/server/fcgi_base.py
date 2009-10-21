3adf17edbd4a278ba7e02eaed4dc555d9729608b
Allan Saddi <allan@saddi.com>
2009-10-21 09:29:33 -0700

    Add configurable timeout (default: no timeout) to be used when the WSGI application is called. Only applies to forked servers!

--- flup/server/fcgi_base.py
+++ flup/server/fcgi_base.py
@@ -533,6 +533,9 @@ class Record(object):
         if self.paddingLength:
             self._sendall(sock, '\x00'*self.paddingLength)
             
+class TimeoutException(Exception):
+    pass
+
 class Request(object):
     """
     Represents a single FastCGI request.
@@ -542,8 +545,9 @@ class Request(object):
     be called by your handler. However, server, params, stdin, stdout,
     stderr, and data are free for your handler's use.
     """
-    def __init__(self, conn, inputStreamClass):
+    def __init__(self, conn, inputStreamClass, timeout):
         self._conn = conn
+        self._timeout = timeout
 
         self.server = conn.server
         self.params = {}
@@ -552,8 +556,20 @@ class Request(object):
         self.stderr = OutputStream(conn, self, FCGI_STDERR, buffered=True)
         self.data = inputStreamClass(conn)
 
+    def timeout_handler(self, signum, frame):
+        self.stderr.write('Timeout Exceeded\n')
+        self.stderr.write("\n".join(traceback.format_stack(frame)))
+        self.stderr.flush()
+
+        raise TimeoutException
+
     def run(self):
         """Runs the handler, flushes the streams, and ends the request."""
+        # If there is a timeout
+        if self._timeout:
+            old_alarm = signal.signal(signal.SIGALRM, self.timeout_handler)
+            signal.alarm(self._timeout)
+            
         try:
             protocolStatus, appStatus = self.server.handler(self)
         except:
@@ -566,6 +582,10 @@ class Request(object):
 
         if __debug__: _debug(1, 'protocolStatus = %d, appStatus = %d' %
                              (protocolStatus, appStatus))
+
+        # Restore old handler if timeout was given
+        if self._timeout:
+            signal.signal(signal.SIGALRM, old_alarm)
 
         try:
             self._flush()
@@ -615,10 +635,11 @@ class Connection(object):
     _multiplexed = False
     _inputStreamClass = InputStream
 
-    def __init__(self, sock, addr, server):
+    def __init__(self, sock, addr, server, timeout):
         self._sock = sock
         self._addr = addr
         self.server = server
+        self._timeout = timeout
 
         # Active Requests for this Connection, mapped by request ID.
         self._requests = {}
@@ -739,7 +760,8 @@ class Connection(object):
         """Handle an FCGI_BEGIN_REQUEST from the web server."""
         role, flags = struct.unpack(FCGI_BeginRequestBody, inrec.contentData)
 
-        req = self.server.request_class(self, self._inputStreamClass)
+        req = self.server.request_class(self, self._inputStreamClass,
+                                        self._timeout)
         req.requestId, req.role, req.flags = inrec.requestId, role, flags
         req.aborted = False
 
@@ -807,8 +829,9 @@ class MultiplexedConnection(Connection):
     _multiplexed = True
     _inputStreamClass = MultiplexedInputStream
 
-    def __init__(self, sock, addr, server):
-        super(MultiplexedConnection, self).__init__(sock, addr, server)
+    def __init__(self, sock, addr, server, timeout):
+        super(MultiplexedConnection, self).__init__(sock, addr, server,
+                                                    timeout)
 
         # Used to arbitrate access to self._requests.
         lock = threading.RLock()
